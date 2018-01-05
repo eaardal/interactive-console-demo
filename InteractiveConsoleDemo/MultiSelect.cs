1a4207212b20f927d2a97b4c5d8aded0088317c4
Eirik Årdal <ekstern_eirik.ardal@spv.no>
2018-01-05 09:08:20 +0100

    Multiselect version that doesnt clear console

--- InteractiveConsoleDemo/MultiSelect.cs
+++ InteractiveConsoleDemo/MultiSelect.cs
@@ -17,7 +17,7 @@ namespace InteractiveConsoleDemo
 
             do
             {
-                (var selectedIndices, var keyInfo) = AskUserToSelectItems(heroes, question);
+                (var selectedIndices, var keyInfo) = AskUserToSelectItems2(heroes, question);
 
                 Console.Clear();
 
@@ -43,7 +43,7 @@ namespace InteractiveConsoleDemo
             } while (mainLoopKeyInfo.Key != ConsoleKey.Escape);
         }
 
-        public static (IReadOnlyCollection<int> selectedIndices, ConsoleKeyInfo keyInfo) AskUserToSelectItems(IReadOnlyCollection<string> items, string question)
+        private static (IReadOnlyCollection<int> selectedIndices, ConsoleKeyInfo keyInfo) AskUserToSelectItems(IReadOnlyCollection<string> items, string question)
         {
             var selectedIndex = 0;
             var selectedIndices = new List<int>();
@@ -58,6 +58,100 @@ namespace InteractiveConsoleDemo
 
                 Console.WriteLine(question);
 
+                for (var i = 0; i < items.Count; i++)
+                {
+                    string text;
+
+                    if (selectedIndex == i)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        text = $"[-] {items.ElementAt(i)}";
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkGreen;
+                        text = $"[-] {items.ElementAt(i)}";
+                    }
+
+                    if (selectedIndices.Contains(i) && selectedIndex == i)
+                    {
+                        Console.ForegroundColor = ConsoleColor.White;
+                        text = $"[+] {items.ElementAt(i)}";
+                    }
+                    else if (selectedIndices.Contains(i))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                        text = $"[+] {items.ElementAt(i)}";
+                    }
+
+                    Console.WriteLine(text);
+                    Console.ResetColor();
+                }
+
+                keyInfo = Console.ReadKey();
+
+                if (keyInfo.Key == ConsoleKey.DownArrow)
+                {
+                    if (selectedIndex + 1 < items.Count)
+                    {
+                        selectedIndex++;
+                    }
+                }
+
+                if (keyInfo.Key == ConsoleKey.UpArrow)
+                {
+                    if (selectedIndex - 1 >= 0)
+                    {
+                        selectedIndex--;
+                    }
+                }
+
+                if (keyInfo.Key == ConsoleKey.Spacebar)
+                {
+                    if (selectedIndices.Contains(selectedIndex))
+                    {
+                        selectedIndices.Remove(selectedIndex);
+                    }
+                    else
+                    {
+                        selectedIndices.Add(selectedIndex);
+                    }
+                }
+
+            } while (keyInfo.Key != ConsoleKey.Escape && keyInfo.Key != ConsoleKey.Enter);
+
+            return (selectedIndices, keyInfo);
+        }
+
+        /// <summary>
+        /// This version does not clear the console for each iteration so text above the list is maintained
+        /// </summary>
+        /// <param name="items"></param>
+        /// <param name="question"></param>
+        /// <returns></returns>
+        private static (IReadOnlyCollection<int> selectedIndices, ConsoleKeyInfo keyInfo) AskUserToSelectItems2(IReadOnlyCollection<string> items, string question)
+        {
+            var selectedIndex = 0;
+            var selectedIndices = new List<int>();
+            var isFirstRun = true;
+
+            Console.CursorVisible = false;
+            
+            Console.WriteLine($"{question} (use SPACE to select and ENTER to finalize)");
+
+            ConsoleKeyInfo keyInfo;
+
+            do
+            {
+                if (isFirstRun)
+                {
+                    isFirstRun = false;
+                }
+                else
+                {
+                    Console.SetCursorPosition(0, Console.CursorTop - items.Count);
+                }
+                
                 for (var i = 0; i < items.Count; i++)
                 {
                     string text;
